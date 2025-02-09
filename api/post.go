723531d99fd0303661a107d2b61c2154f05f1e10
David Fryer <dfryer1193@gmail.com>
2025-02-08 22:56:06 -0500

    Remove comments from post model

--- api/post.go
+++ api/post.go
@@ -1,10 +1,9 @@
 package api
 
 type Post struct {
-	ID        string    `json:"id"`
-	Title     string    `json:"title"`
-	Content   string    `json:"content"`
-	CreatedAt string    `json:"created_at"`
-	UpdatedAt string    `json:"updated_at"`
-	Comments  []Comment `json:"comments"`
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
 }
