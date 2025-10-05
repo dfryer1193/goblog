f3c33b49a4d91fd2c41cd10b5d6fe38cec9650e4
David Fryer <dfryer1193@gmail.com>
2025-10-04 20:14:27 -0400

    Remove old code

--- api/post.go
+++
@@ -1,9 +0,0 @@
-package api
-
-type Post struct {
-	ID        string `json:"id"`
-	Title     string `json:"title"`
-	Content   string `json:"content"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
--- internal/data/db.go
+++
@@ -1,7 +0,0 @@
-package data
-
-type DB struct{}
-
-func NewDB() *DB {
-	return &DB{}
-}
