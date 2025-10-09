31773a8695cc7c68013837d75387e85eaf668477
David Fryer <dfryer1193@gmail.com>
2025-10-09 16:55:26 -0400

    Add some stub data

---
+++ blog/application/markdown.go
@@ -0,0 +1,18 @@
+package application
+
+// MarkdownRenderer defines the interface for converting markdown to HTML.
+type MarkdownRenderer interface {
+	Render(markdown []byte) (string, error)
+}
+
+// DummyMarkdownRenderer is a dummy implementation of MarkdownRenderer for testing and MVP.
+// It simply wraps the markdown content in <pre> tags.
+type DummyMarkdownRenderer struct{}
+
+func NewDummyMarkdownRenderer() *DummyMarkdownRenderer {
+	return &DummyMarkdownRenderer{}
+}
+
+func (r *DummyMarkdownRenderer) Render(markdown []byte) (string, error) {
+	return "<pre>" + string(markdown) + "</pre>", nil
+}
