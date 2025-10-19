3b9466b2bdb6aa63bc78dbe32170eb2f48c59676
David Fryer <dfryer1193@gmail.com>
2025-10-19 19:52:00 -0400

    Set up initial markdown renderer scaffolding

--- blog/application/markdown.go
+++ blog/application/markdown.go
@@ -1,5 +1,14 @@
 package application
 
+import (
+	"bytes"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+)
+
 // MarkdownProcessingResult contains the results of processing a markdown file
 type MarkdownProcessingResult struct {
 	Title    string
@@ -14,18 +23,43 @@ type MarkdownRenderer interface {
 	Render(markdown []byte) (*MarkdownProcessingResult, error)
 }
 
-// DummyMarkdownRenderer is a dummy implementation of MarkdownRenderer for testing and MVP.
-// It simply wraps the markdown content in <pre> tags.
-type DummyMarkdownRenderer struct{}
-
-func NewDummyMarkdownRenderer() *DummyMarkdownRenderer {
-	return &DummyMarkdownRenderer{}
+type MarkdownRendererImpl struct {
+	renderer goldmark.Markdown
 }
 
-func (r *DummyMarkdownRenderer) Render(markdown []byte) (*MarkdownProcessingResult, error) {
-	return &MarkdownProcessingResult{
-		Title:    "Untitled",
-		Snippet:  "",
-		HTMLPath: "",
-	}, nil
+func NewMarkdownRenderer() MarkdownRenderer {
+	// TODO: Implement custom domains for relative links
+	renderer := goldmark.New(
+		goldmark.WithExtensions(
+			extension.GFM,
+			extension.Table,
+			extension.Strikethrough,
+			extension.TaskList,
+		),
+		goldmark.WithParserOptions(
+			parser.WithAutoHeadingID(),
+		),
+		goldmark.WithRendererOptions(
+			html.WithHardWraps(),
+			html.WithXHTML(),
+			html.WithUnsafe(),
+		),
+	)
+
+	return &MarkdownRendererImpl{
+		renderer: renderer,
+	}
+}
+
+func (r *MarkdownRendererImpl) Render(markdown []byte) (*MarkdownProcessingResult, error) {
+	var buf bytes.Buffer
+	r.renderer.Convert(markdown, &buf)
+
+	// TODO: pass filename in and convert it to html
+	// TODO: Write html content to disk
+	// TODO: Extract title
+	// TODO: extract snippet
+	// TODO: build & return MarkdownProcessingResult
+
+	return nil, nil
 }
