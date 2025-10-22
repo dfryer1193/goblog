e5032deb2b3f11c66ddf290f3138fd5ac6fd031e
David Fryer <dfryer1193@gmail.com>
2025-10-21 20:31:49 -0400

    Implement more pieces of markdown renderer

--- blog/application/markdown.go
+++ blog/application/markdown.go
@@ -1,7 +1,8 @@
 package application
 
 import (
-	"bytes"
+	"os"
+	"strings"
 
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark/extension"
@@ -20,14 +21,15 @@ type MarkdownProcessingResult struct {
 
 // MarkdownRenderer defines the interface for converting markdown to HTML.
 type MarkdownRenderer interface {
-	Render(markdown []byte) (*MarkdownProcessingResult, error)
+	Render(basename string, markdown []byte) (*MarkdownProcessingResult, error)
 }
 
 type MarkdownRendererImpl struct {
+	postDir  string
 	renderer goldmark.Markdown
 }
 
-func NewMarkdownRenderer() MarkdownRenderer {
+func NewMarkdownRenderer(postDir string) MarkdownRenderer {
 	// TODO: Implement custom domains for relative links
 	renderer := goldmark.New(
 		goldmark.WithExtensions(
@@ -47,19 +49,57 @@ func NewMarkdownRenderer() MarkdownRenderer {
 	)
 
 	return &MarkdownRendererImpl{
+		postDir:  postDir,
 		renderer: renderer,
 	}
 }
 
-func (r *MarkdownRendererImpl) Render(markdown []byte) (*MarkdownProcessingResult, error) {
-	var buf bytes.Buffer
-	r.renderer.Convert(markdown, &buf)
-
-	// TODO: pass filename in and convert it to html
-	// TODO: Write html content to disk
-	// TODO: Extract title
+func (r *MarkdownRendererImpl) Render(basename string, markdown []byte) (*MarkdownProcessingResult, error) {
+	title := extractPostTitle(markdown)
 	// TODO: extract snippet
-	// TODO: build & return MarkdownProcessingResult
+	snippet := extractSnippet(markdown)
+	htmlBasename := strings.Replace(basename, ".md", ".html", 1)
+	postPath := r.postDir + "/" + htmlBasename
 
-	return nil, nil
+	file, err := os.Create(postPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	r.renderer.Convert(markdown, file)
+	err = file.Sync()
+	if err != nil {
+		return nil, err
+	}
+
+	return &MarkdownProcessingResult{
+		Title:    title,
+		Snippet:  snippet,
+		HTMLPath: htmlBasename,
+	}, nil
+}
+
+func extractPostTitle(markdown []byte) string {
+	lines := strings.SplitN(string(markdown), "\n", 2)
+	if len(lines) == 0 {
+		return "Untitled Post"
+	}
+
+	firstLine := strings.TrimSpace(lines[0])
+	if strings.HasPrefix(firstLine, "# ") {
+		return strings.TrimSpace(strings.TrimPrefix(firstLine, "# "))
+	}
+
+	return "Untitled Post"
+}
+
+func extractSnippet(markdown []byte) string {
+	lines := strings.SplitN(string(markdown), "\n", 3)
+	if len(lines) < 2 {
+		return ""
+	}
+
+	secondLine := strings.TrimSpace(lines[1])
+	return secondLine
 }
